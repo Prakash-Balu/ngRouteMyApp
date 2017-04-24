b9141dcf71d24a73ff5c7949fb92a8d8b4b4649c
Prakash-Balu <prakash.engg89@gmail.com>
2017-04-24 16:07:20 +0530

    updated

--- app/js/app.js
+++ app/js/app.js
@@ -1,25 +1,4 @@
-var app = angular.module('motiveReports', ['ui.router', 'ngRoute' ]);
-
-/*app.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
-  // This is required for Browser Sync to work poperly
-  //$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-  // 
-  // For any unmatched url, redirect to /state1 
-  $urlRouterProvider.otherwise("/signin");
-  // 
-  // Now set up the states 
-  $stateProvider
-    .state('signin', {
-      url: "/signin",
-      templateUrl: "signin.html",
-	  controller: 'loginCtrl'
-    })
-    .state('home', {
-      url: "/home",
-      templateUrl: "home.html"
-    });
-});*/
-
+var app = angular.module('motiveReports', ['ngRoute' ]);
 
 app.config(function($routeProvider) {
   $routeProvider.
